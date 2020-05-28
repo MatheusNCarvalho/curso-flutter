8b7fa8261a0e661f933423aa544fd508c09fc643
Matheus Neves <matheus.carvalho@teckavan.com>
2020-05-28 16:41:38 -0300

    curso

--- loja_virtual/lib/pages/home_page.dart
+++ loja_virtual/lib/pages/home_page.dart
@@ -1,13 +1,17 @@
 
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/tabs/home_tab.dart';
 
 class HomePage extends StatelessWidget {
+  final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-        Container(color: Colors.yellow,),
-        Container(color: Colors.red,),
+        HomeTab()
       ],
     );
   }
