dfc6e759b1dc1c5b547d377d3e66c6386fde04dd
Matheus Neves <matheus.carvalho@teckavan.com>
2020-06-01 13:36:07 -0300

    curso

--- loja_virtual/lib/pages/home_page.dart
+++ loja_virtual/lib/pages/home_page.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/tabs/home_tab.dart';
+import 'package:lojavirtual/tabs/products_tab.dart';
 import 'package:lojavirtual/widgets/custom_drawer.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,11 +14,17 @@ class HomePage extends StatelessWidget {
       children: <Widget>[
         Scaffold(
           body: HomeTab(),
-          drawer: CustomDrawer(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab()
         )
-
       ],
     );
   }
 }
-
--- loja_virtual/lib/widgets/custom_drawer.dart
+++ loja_virtual/lib/widgets/custom_drawer.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/widgets/drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
+  final PageController pageController;
+
+  CustomDrawer(this.pageController);
+
   @override
   Widget build(BuildContext context) {
     Widget _buildDrawerBack() => Container(
@@ -33,10 +38,59 @@ class CustomDrawer extends StatelessWidget {
                         style: TextStyle(
                             fontSize: 34.0, fontWeight: FontWeight.bold),
                       ),
+                    ),
+                    Positioned(
+                      left: 0.0,
+                      bottom: 0.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Olá",
+                            style: TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold),
+                          ),
+                          GestureDetector(
+                            child: Text(
+                              "Entre ou cadastra-se >",
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onTap: () {},
+                          )
+                        ],
+                      ),
                     )
                   ],
                 ),
-              )
+              ),
+              Divider(),
+              DrawerTile(
+                this.pageController,
+                icon: Icons.home,
+                text: "Início",
+                page: 0,
+              ),
+              DrawerTile(
+                this.pageController,
+                icon: Icons.list,
+                text: "Produtos",
+                page: 1,
+              ),
+              DrawerTile(
+                this.pageController,
+                icon: Icons.location_on,
+                text: "Lojas",
+                page: 2,
+              ),
+              DrawerTile(
+                this.pageController,
+                icon: Icons.playlist_add_check,
+                text: "Meus pedidos",
+                page: 3,
+              ),
             ],
           )
         ],
