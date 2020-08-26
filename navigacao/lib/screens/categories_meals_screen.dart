80760013cf6b65996ff133859a522ddab53a59f3
Matheus <matheusnevesdecarvalho@gmail.com>
2020-08-25 22:42:43 -0300

    curso

--- navigacao/lib/screens/categories_meals_screen.dart
+++ navigacao/lib/screens/categories_meals_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:navigacao/components/meal_item.dart';
+import 'package:navigacao/data/dummy_data.dart';
 import 'package:navigacao/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
@@ -6,13 +8,21 @@ class CategoriesMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context).settings.arguments as Category;
 
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category?.id ?? null);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category?.title ?? ""),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text("Receitas por Categoria"),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (context, index) {
+          final meal = categoryMeals[index];
+          return MealItem(meal: meal);
+        },
       ),
     );
   }
