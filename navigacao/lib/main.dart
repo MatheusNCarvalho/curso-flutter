90610e44536e0cbabe16f81fb7715455375b4422
Matheus <matheusnevesdecarvalho@gmail.com>
2020-08-25 22:11:57 -0300

    ajustes

--- navigacao/lib/main.dart
+++ navigacao/lib/main.dart
@@ -1,38 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:navigacao/screens/categories_meals_screen.dart';
+import 'package:navigacao/screens/categories_screen.dart';
+import 'package:navigacao/utils/app_routes.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {  
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vamos Cozinhar?',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Vamos Cozinha?"),
-      ),
-      body: Center(
-        child: Text("Navegar é preciso!!"),
-      ),
+      routes: {
+        AppRoutes.HOME: (_) => CategioriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (_) => CategoriesMealsScreen(),
+      },
     );
   }
 }
---
+++ navigacao/lib/screens/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:navigacao/components/category_item.dart';
+import 'package:navigacao/data/dummy_data.dart';
+
+class CategioriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Vamos Conzinhar?"),
+        centerTitle: true,
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((category) {
+          return CategoryItem(category: category);
+        }).toList(),
+      ),
+    );
+  }
+}
