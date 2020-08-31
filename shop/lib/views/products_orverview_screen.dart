afe54c1391f2ce6d726d45144a938eec24c28e03
Matheus <matheusnevesdecarvalho@gmail.com>
2020-08-30 22:32:25 -0300

    curso

---
+++ shop/lib/views/products_orverview_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+
+class ProductOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
