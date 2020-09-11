0589d7da5049a61e8c0ab2308f33adc8b1146ced
Matheus <matheusnevesdecarvalho@gmail.com>
2020-09-10 23:02:03 -0300

    curso

---
+++ arch/lib/app/app_widget.dart
@@ -0,0 +1,24 @@
+import 'package:arch/app/app_controller.dart';
+import 'package:arch/app/pages/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: AppController.instance.themeSwitch,
+      builder: (context, isDart, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            brightness: isDart ? Brightness.dark : Brightness.light
+          ),
+          home: HomePage(),
+        );
+      },
+    );
+  }
+}
---
+++ arch/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:arch/app/app_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());}
+
+
